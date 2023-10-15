db88f4141146a779a971509685b9d6e3b6e16698
sushmitha-sushii <sushmithas.bsc23@rvu.edu.in>
2023-10-15 22:14:37 +0530

    committed 10/15/2023, 10:14:37 PM

--- set01/problem04.c
+++ set01/problem04.c
@@ -5,9 +5,25 @@ void add(int a, int b, int *sum);
 void output(int a, int b, int sum);
 int main()
 {
-
+ int a , b , sum=0;
+ input(&a,&b);
+ add(a,b,&sum);
+ output(a,b,sum);
+ return 0 ;
 }
 void input(int *a, int *b)
 {
-    
+    printf("Enter the first number: \n");
+    scanf("%d",a);
+    printf("Enter the second number: \n");
+    scanf("%d",b);
 }
+void add(int a, int b, int *sum)
+{
+    *sum=a+b;
+}
+void output(int a, int b, int sum)
+{
+    printf("The sum of %d and %d is %d",a,b,sum);
+}
+
