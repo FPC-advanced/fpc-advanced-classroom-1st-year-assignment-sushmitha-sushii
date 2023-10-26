7758ada4dc6d5731476de3050537343d15bf5b38
sushmitha-sushii <sushmithas.bsc23@rvu.edu.in>
2023-10-26 16:08:46 +0530

    committed 10/26/2023, 4:08:46 PM

--- set02/problem02.c
+++ set02/problem02.c
@@ -1,15 +1,18 @@
 //To find if a triangle is scalene
+#include<stdio.h>
 int input_side();
 int check_scalene(int a, int b, int c);
 void output(int a, int b, int c, int isscalene);
 int main()
 {
     int a , b , c , isscalene;
-    a=input();
-    b=input();
-    c=input();
-
-
+    a=input_side();
+    b=input_side();
+    c=input_side();
+    isscalene=check_scalene(a,b,c);
+    output(a,b,c,isscalene);
+    return 0;
+    
 }
 int input_side()
 {
@@ -20,7 +23,23 @@ int input_side()
 }
 int check_scalene(int a, int b, int c)
 {
-    int isscalene;
-    
-
+  if(a!=b && a!=c && b!=c)
+    {
+        return 1;
+    }
+        else
+        {
+            return 0;
+        }
+ }
+void output(int a, int b, int c, int isscalene)
+{
+    if(isscalene)
+    {
+    printf("The triangle with sides %d ,%d and %d is scalene",a,b,c);
+    }
+    else
+    {
+    printf("The triangle with sides %d ,%d and %d is not scalene",a,b,c);
+    }
 }
