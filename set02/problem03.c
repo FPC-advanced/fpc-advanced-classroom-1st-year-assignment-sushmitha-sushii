6443d90d813a9aaeb6d959c85b5858049acf9b70
sushmitha-sushii <sushmithas.bsc23@rvu.edu.in>
2023-10-26 16:16:18 +0530

    committed 10/26/2023, 4:16:17 PM

---
+++ set02/problem03.c
@@ -0,0 +1,19 @@
+//  To find whether a number is a composite number
+#include<stdio.h>
+int input_number();
+int is_composite(int n);
+void output(int n, int result);
+int main()
+{
+    int n , result;
+    n=input_number();
+
+}
+int input_number()
+{
+    int x;
+    printf("Enter a number: \n");
+    scanf("%d",x);
+    return x;
+}
+
