7fc246b5b145fce585eb3538715ae88591418811
sushmitha-sushii <sushmithas.bsc23@rvu.edu.in>
2023-10-26 16:45:58 +0530

    committed 10/26/2023, 4:45:57 PM

--- set02/problem03.c
+++ set02/problem03.c
@@ -7,13 +7,41 @@ int main()
 {
     int n , result;
     n=input_number();
-
+    result=is_composite(n);
+    output(n,result);
+    return 0 ;
 }
 int input_number()
 {
     int x;
     printf("Enter a number: \n");
-    scanf("%d",x);
+    scanf("%d",&x);
     return x;
+}
+int is_composite(int n)
+{
+    int i;
+    if(n<=1)
+    {
+        return 0;
+    }
+    else
+    {
+        for(i=2;i<n;i++)
+        if(n%i==0)
+         return 1;
+    }
+    
+}
+void output(int n, int result)
+{
+    if(result)
+    {
+        printf("%d is a composite number",n);
+    }
+    else
+    {
+       printf("%d is not a composite number",n);
+    }
 }
 
