0bf38a9a44ee5c3c7df7cf8b53cd81e7ed19da51
sushmitha-sushii <sushmithas.bsc23@rvu.edu.in>
2023-11-26 15:37:31 +0530

    committed 11/26/2023, 3:37:29 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -17,10 +17,25 @@ void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float
 {
     printf("Enter coordinates of first point (x y): ");
     scanf("%f %f", x1, y1);
-
     printf("Enter coordinates of second point (x y): ");
     scanf("%f %f", x2, y2);
-
     printf("Enter coordinates of third point (x y): ");
     scanf("%f %f", x3, y3);
+}
+float distance(float x1, float y1, float x2, float y2) {
+    return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+}
+
+int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3) {
+    float side1, side2, side3;
+
+    side1 = distance(x1, y1, x2, y2);
+    side2 = distance(x2, y2, x3, y3);
+    side3 = distance(x3, y3, x1, y1);
+
+    if (side1 + side2 > side3 && side2 + side3 > side1 && side3 + side1 > side2) {
+        return 1; // It's a triangle
+    } else {
+        return 0; // It's not a triangle
+    }
 }
