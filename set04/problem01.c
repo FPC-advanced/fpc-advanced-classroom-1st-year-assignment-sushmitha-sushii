19ea3a6961ea0b979eccc11c8c8013865d642f2b
sushmitha-sushii <sushmithas.bsc23@rvu.edu.in>
2023-11-26 22:02:20 +0530

    committed 11/26/2023, 10:02:19 PM

--- set04/problem01.c
+++ set04/problem01.c
@@ -1,1 +1,9 @@
 //To find sum of two fractions
+#include<stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2);
+void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den);
+void output(int num1, int den1, int num2, int den2, int res_num, int res_den);
+int main()
+{
+    
+}
