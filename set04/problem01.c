98ec6a9dedf22caa84f247a6a03205f007b319a1
sushmitha-sushii <sushmithas.bsc23@rvu.edu.in>
2023-11-26 22:03:20 +0530

    committed 11/26/2023, 10:03:19 PM

--- set04/problem01.c
+++ set04/problem01.c
@@ -5,5 +5,14 @@ void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den);
 void output(int num1, int den1, int num2, int den2, int res_num, int res_den);
 int main()
 {
-    
+    int num1, den1, num2, den2, res_num, res_den;
+    input(&num1, &den1, &num2, &den2);
+
+    // Add the fractions
+    add(num1, den1, num2, den2, &res_num, &res_den);
+
+    // Output the fractions and their sum
+    output(num1, den1, num2, den2, res_num, res_den);
+
+    return 0;
 }
