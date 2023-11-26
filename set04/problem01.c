7272781c8a3dfb6728b0381dc042e90744e55df6
sushmitha-sushii <sushmithas.bsc23@rvu.edu.in>
2023-11-26 22:04:50 +0530

    committed 11/26/2023, 10:04:49 PM

--- set04/problem01.c
+++ set04/problem01.c
@@ -23,7 +23,8 @@ void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den)
     *res_num = (num1 * den2) + (num2 * den1);
     *res_den = den1 * den2;
 }
-void output(int num1, int den1, int num2, int den2, int res_num, int res_den) {
+void output(int num1, int den1, int num2, int den2, int res_num, int res_den)
+{
     printf("The sum of %d/%d and %d/%d is: %d/%d\n", num1, den1, num2, den2, res_num, res_den);
 }
 
