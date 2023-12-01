20a634b807bf30d1b61df5dbea4d9a51db777e71
sushmitha-sushii <sushmithas.bsc23@rvu.edu.in>
2023-12-01 20:31:34 +0530

    committed 12/1/2023, 8:31:34 PM

--- set03/problem07.c
+++ set03/problem07.c
@@ -1,27 +1,17 @@
 //To find the length of a line
 #include <stdio.h>
 #include <math.h>
-int main() 
-{
-    Line line = input_line();
-    find_length(&line);
-    output(line);
-    return 0;
-}
-typedef struct point 
-{
-float x, y;
-} 
-Point;
-typedef struct line 
-{
+
+typedef struct Point {
+    float x, y;
+} Point;
+
+typedef struct Line {
     Point p1, p2;
     float distance;
-} 
-Line;
+} Line;
 
-Point input_point() 
-{
+Point input_point() {
     Point p;
     printf("Enter x coordinate: ");
     scanf("%f", &p.x);
@@ -29,8 +19,8 @@ Point input_point()
     scanf("%f", &p.y);
     return p;
 }
-Line input_line() 
-{
+
+Line input_line() {
     Line l;
     printf("Enter coordinates for Point 1:\n");
     l.p1 = input_point();
@@ -38,16 +28,21 @@ Line input_line()
     l.p2 = input_point();
     return l;
 }
-void find_length(Line *l) 
-{
+
+void find_length(Line *l) {
     float dx = l->p2.x - l->p1.x;
     float dy = l->p2.y - l->p1.y;
     l->distance = sqrt(dx * dx + dy * dy);
 }
+
 void output(Line l) {
     printf("Length of the line between (%.2f, %.2f) and (%.2f, %.2f) is: %.2f\n",
            l.p1.x, l.p1.y, l.p2.x, l.p2.y, l.distance);
 }
 
-
-
+int main() {
+    Line line = input_line();
+    find_length(&line);
+    output(line);
+    return 0;
+}
