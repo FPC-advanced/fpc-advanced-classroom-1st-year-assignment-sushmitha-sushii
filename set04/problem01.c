14001e838f15d7f9692b1b97488c61a64921c8ad
sushmitha-sushii <sushmithas.bsc23@rvu.edu.in>
2023-12-01 17:37:42 +0530

    committed 12/1/2023, 5:37:41 PM

--- set04/problem01.c
+++ set04/problem01.c
@@ -9,22 +9,21 @@ int main()
     input(&num1, &den1, &num2, &den2);
     add(num1, den1, num2, den2, &res_num, &res_den);
     output(num1, den1, num2, den2, res_num, res_den);
-     return 0;
+    return 0;
 }
 void input(int *num1, int *den1, int *num2, int *den2) 
 {
-    printf("Enter first fraction (numerator/denominator): ");
+    printf("Enter the first fraction (numerator/denominator): ");
     scanf("%d/%d", num1, den1);
-    printf("Enter second fraction (numerator/denominator): ");
-    scanf("%d/%d", num2, den2);
+    printf("Enter the second fraction (numerator/denominator): ");
+    scanf("%d/%d", num2, den2); 
 }
 void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den) 
 {
-    *res_num = (num1 * den2) + (num2 * den1);
     *res_den = den1 * den2;
+    *res_num = (num1 * den2) + (num2 * den1);
 }
 void output(int num1, int den1, int num2, int den2, int res_num, int res_den)
 {
-    printf("The sum of %d/%d and %d/%d is: %d/%d\n", num1, den1, num2, den2, res_num, res_den);
+    printf("Sum of %d/%d and %d/%d is %d/%d\n", num1, den1, num2, den2, res_num, res_den);
 }
-
