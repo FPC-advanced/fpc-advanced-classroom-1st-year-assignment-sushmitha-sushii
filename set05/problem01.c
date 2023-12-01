0860e879e0adc7d6a90d312d71c7c1d8be9daa22
sushmitha-sushii <sushmithas.bsc23@rvu.edu.in>
2023-12-01 17:44:28 +0530

    committed 12/1/2023, 5:44:27 PM

--- set05/problem01.c
+++ set05/problem01.c
@@ -1,9 +1,10 @@
 //To find the distance between two points.
 #include<stdio.h>
 #include<math.h>
-struct_point
+struct _point
 {
   float x;
   float y;
 };
-typedef struct_point Point;
+typedef struct _point Point;
+
