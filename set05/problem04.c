3c110774f668cbfdb4e1f3e0710a7153f41cd9e0
sushmitha-sushii <sushmithas.bsc23@rvu.edu.in>
2023-12-01 19:06:52 +0530

    committed 12/1/2023, 7:06:52 PM

--- set05/problem04.c
+++ set05/problem04.c
@@ -1,22 +1,19 @@
 //Write a program to find out the mood of a Camel.
 #include <stdio.h>
-
 void input_camel_details(float *radius, float *height, float *length);
 int find_mood(float radius, float height, float length);
 void output(float radius, float height, float length, int mood);
-
-int main() {
+int main() 
+{
     float stomach_radius, camel_height, camel_length;
     int mood;
-
     input_camel_details(&stomach_radius, &camel_height, &camel_length);
     mood = find_mood(stomach_radius, camel_height, camel_length);
     output(stomach_radius, camel_height, camel_length, mood);
-
     return 0;
 }
-
-void input_camel_details(float *radius, float *height, float *length) {
+void input_camel_details(float *radius, float *height, float *length)
+ {
     printf("Enter stomach radius of the camel: ");
     scanf("%f", radius);
     printf("Enter height of the camel: ");
@@ -24,8 +21,8 @@ void input_camel_details(float *radius, float *height, float *length) {
     printf("Enter length of the camel: ");
     scanf("%f", length);
 }
-
-int find_mood(float radius, float height, float length) {
+int find_mood(float radius, float height, float length) 
+{
     int mood = -1; 
     if (radius < height && radius < length)
         mood = 0; 
@@ -33,11 +30,10 @@ int find_mood(float radius, float height, float length) {
         mood = 1; 
     else if (length < height && length < radius)
         mood = 2; 
-
     return mood;
 }
-
-void output(float radius, float height, float length, int mood) {
+void output(float radius, float height, float length, int mood) 
+{
     printf("Stomach Radius: %.2f\n", radius);
     printf("Height: %.2f\n", height);
     printf("Length: %.2f\n", length);
