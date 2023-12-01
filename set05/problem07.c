0dcbcfe024fd09f84cf85df47feb4ad557a6ca1d
sushmitha-sushii <sushmithas.bsc23@rvu.edu.in>
2023-12-01 18:18:15 +0530

    committed 12/1/2023, 6:18:14 PM

--- set05/problem07.c
+++ set05/problem07.c
@@ -7,26 +7,27 @@ int main()
 {
     char camel_name[50];
     input(camel_name);
-
     int result = has_nice_name(camel_name);
-
     output(result);
-
     return 0;
 }
-void input(char *name) {
+void input(char *name) 
+{
     printf("Enter the name of the camel: ");
     scanf("%s", name);
 }
-int is_vowel(char c) {
+int is_vowel(char c) 
+{
     c = (char)tolower(c);  
     return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
 }
-int has_nice_name(char *c) {
+int has_nice_name(char *c) 
+{
     int vowels = 0, consonants = 0;
-
-    for (int i = 0; c[i] != '\0'; i++) {
-        if (is_vowel(c[i])) {
+    for (int i = 0; c[i] != '\0'; i++) 
+    {
+        if (is_vowel(c[i])) 
+        {
             vowels++;
         } else if (isalpha(c[i])) {
             consonants++;
