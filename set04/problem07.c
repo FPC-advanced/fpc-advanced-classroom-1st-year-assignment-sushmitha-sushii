daa23cc200acc383e861472b10ae500ec2bd83e2
sushmitha-sushii <sushmithas.bsc23@rvu.edu.in>
2023-12-05 09:11:54 +0530

    committed 12/5/2023, 9:11:53 AM

--- set04/problem07.c
+++ set04/problem07.c
@@ -31,20 +31,18 @@ int find_gcd(int a, int b)
         return a;
     return find_gcd(b, a % b);
 }
-Fraction add_fractions(Fraction f1, Fraction f2) {
+Fraction add_fractions(Fraction f1, Fraction f2) 
+{
     Fraction result;
     result.num = f1.num * f2.den + f2.num * f1.den;
     result.den = f1.den * f2.den;
-
-    
     int gcd = find_gcd(result.num, result.den);
     result.num /= gcd;
     result.den /= gcd;
-
-    return result;
+   return result;
 }
-
-void  output(Fraction f1, Fraction f2, Fraction sum) {
+void  output(Fraction f1, Fraction f2, Fraction sum) 
+{
     printf("\nResult of adding fractions %d/%d and %d/%d is: %d/%d\n",
            f1.num, f1.den, f2.num, f2.den, sum.num, sum.den);
 }
